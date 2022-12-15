06e450366b9462a7cb3dc15ca45da7592c0f6bf2
Jose Commins <axora@axora.net>
2022-12-15 17:14:15 +0000

    Added support for empty files and directories.

--- file-repacker.py
+++ file-repacker.py
@@ -4,6 +4,7 @@ from enum import Enum, auto
 import os
 import io
 import shutil
+import tempfile
 import datetime
 import py7zr
 import zipfile
@@ -46,90 +47,91 @@ def process_files(filename: str, current_relative_path: str, current_path: str, 
     # Set post-processing default to copy file; this can be changed by any intermediate processing.
     # If any exceptions occur in the compression libraries, the process will default copy.
     post_file_processing = FileOperations.COPY
-    destination_archive_filename = None
 
     file_details = os.path.splitext(filename)
     file_basename = file_details[0]
     file_extension = file_details[1].casefold()
     file_size = os.path.getsize(f"{current_path}{filename}")
-    logging.info(f":{process_pid}: + Processing file [{current_path}{filename}] {human_readable_bytes(file_size)}")
+    logging.info(f":PID-{process_pid}: + Processing file [{current_path}{filename}] {human_readable_bytes(file_size)}")
+
+    # Temporary (zero-legth read) in-memory file as py7zr currently cannot add an empty file from an existing archive.
+    empty_file = io.BytesIO(b'0')
+    empty_file.seek(1)
  
     if file_size > max_file_size:
-        logging.warning(f":{process_pid}: Direct copy of file [{current_path}{filename}] as file size of {human_readable_bytes(file_size)} exceeds configuration maximum of {human_readable_bytes(max_file_size)}.")
+        logging.warning(f":PID-{process_pid}: Direct copy of file [{current_path}{filename}] as file size of {human_readable_bytes(file_size)} exceeds configuration maximum of {human_readable_bytes(max_file_size)}.")
         post_file_processing = FileOperations.COPY
     elif file_extension in file_extensions_to_compress:
-        logging.info(f":{process_pid}: Compressing: [{current_path}{file_basename}{file_extension}]")
+        logging.info(f":PID-{process_pid}: Compressing: [{current_path}{file_basename}{file_extension}]")
         os.makedirs(f"{destination_directory}{current_relative_path}", exist_ok=True)
         destination_archive_filename = f"{destination_directory}{current_relative_path}{file_basename}.7z"
+        compressed_archive = py7zr.SevenZipFile(destination_archive_filename, mode="w", filters=compression_filters)
         # -- Check if file is 7zip.
         if file_extension == ".7z":
             try:
-                archive = py7zr.SevenZipFile(f"{current_path}{filename}", mode="r")
-                # Check if the archive has any compressed files.
-                compressed_size = 0
-                for archive_details in archive.list():
-                    compressed_size += archive_details.compressed
-                if compressed_size > 0:
-                    recompressed_archive = py7zr.SevenZipFile(destination_archive_filename,
-                        mode="w", filters=compression_filters)
-                    for archive_filename, binary in archive.readall().items():
-                        logging.info(f":{process_pid}: Archive content in [{filename}]: /{archive_filename}")
-                        recompressed_archive.writef(binary, archive_filename)
-                    recompressed_archive.close()
-                    post_file_processing = FileOperations.DO_NOTHING
-                else:
-                    logging.warning(f":{process_pid}: No files found in archive [{current_path}{filename}] Copying archive instead.")
-                    post_file_processing = FileOperations.COPY
+                source_archive = py7zr.SevenZipFile(f"{current_path}{filename}", mode="r")
+                for archive_info in source_archive.list():
+                    logging.info(f":PID-{process_pid}: Archive content in [{filename}]: /{archive_info.filename}")
+                    # Currently py7zr doesn't support adding empty directories directly, so use tmpfs (https://github.com/miurahr/py7zr/issues/412).
+                    if archive_info.is_directory is True:
+                        with tempfile.TemporaryDirectory(prefix="file-repacker-") as temp_file:
+                            compressed_archive.write(temp_file, f"{archive_info.filename}")
+                    elif archive_info.uncompressed == 0:
+                        compressed_archive.writef(empty_file, f"{archive_info.filename}")
+                    else:
+                        binary = source_archive.read([archive_info.filename])
+                        compressed_archive.writef(binary.get(archive_info.filename), archive_info.filename)
+                        source_archive.reset()
+                logging.info(f":PID-{process_pid}: Recompressed 7zip: [{destination_archive_filename}]")
+                post_file_processing = FileOperations.DO_NOTHING
             # Catch-all for any library quirks.
             except Exception:
-                logger.exception(f":{process_pid}: Exception in processing 7zip file: [{current_path}{filename}] Copying archive instead.")
+                logger.exception(f":PID-{process_pid}: Exception in processing 7zip file: [{current_path}{filename}] Copying archive instead.")
                 post_file_processing = FileOperations.COPY
             finally:
-                archive.close()
+                source_archive.close()
+                compressed_archive.close()
         # -- Check if file is Zip.
         elif file_extension == ".zip":
             try:
-                archive = zipfile.ZipFile(f"{current_path}{filename}", mode="r")
-                # Check if the archive has any compressed files.
-                compressed_size = 0
-                for archive_details in archive.infolist():
-                    compressed_size += archive_details.compress_size
-                if compressed_size > 0:
-                    recompressed_archive = py7zr.SevenZipFile(destination_archive_filename,
-                        mode="w", filters=compression_filters)
-                    for archive_info in archive.infolist():
-                        if archive_info.file_size > 0:
-                            logging.info(f":{process_pid}: Archive content in [{filename}]: /{archive_info.filename}")
-                            binary = io.BytesIO(archive.read(name=archive_info.filename))
-                            recompressed_archive.writef(binary, archive_info.filename)
-                    recompressed_archive.close()
-                    post_file_processing = FileOperations.DO_NOTHING
-                else:
-                    logging.warning(f":{process_pid}: No files found in archive [{current_path}{filename}] Copying archive instead.")
-                    post_file_processing = FileOperations.COPY
+                source_archive = zipfile.ZipFile(f"{current_path}{filename}", mode="r")
+                for archive_info in source_archive.infolist():
+                    logging.info(f":PID-{process_pid}: Archive content in [{filename}]: /{archive_info.filename}")
+                    # Currently py7zr doesn't support adding empty directories directly, so use tmpfs (https://github.com/miurahr/py7zr/issues/412).
+                    if archive_info.is_dir() is True:
+                        with tempfile.TemporaryDirectory(prefix="file-repacker-") as temp_file:
+                            compressed_archive.write(temp_file, f"{archive_info.filename}")
+                    elif archive_info.file_size == 0:
+                        compressed_archive.writef(empty_file, f"{archive_info.filename}")
+                    else:
+                        binary = io.BytesIO(source_archive.read(name=archive_info.filename))
+                        compressed_archive.writef(binary, archive_info.filename)
+                logging.info(f":PID-{process_pid}: Recompressed zip: [{destination_archive_filename}]")
+                post_file_processing = FileOperations.DO_NOTHING
             # Catch-all for any library quirks.
             except Exception:
-                logger.exception(f":{process_pid}: Exception in processing Zip file: [{current_path}{filename}] Copying archive instead.")
+                logger.exception(f":PID-{process_pid}: Exception in processing Zip file: [{current_path}{filename}] Copying archive instead.")
                 post_file_processing = FileOperations.COPY
             finally:
-                archive.close()
+                source_archive.close()
+                compressed_archive.close()
         # Otherwise compress files with any specified extensions.
         else:
             os.makedirs(f"{destination_directory}{current_relative_path}", exist_ok=True)
-            compressed_archive = py7zr.SevenZipFile(f"{destination_archive_filename}", mode="w", filters=compression_filters)
             compressed_archive.write(f"{current_path}{filename}", arcname=filename)
-            compressed_archive.close()
             post_file_processing = FileOperations.DO_NOTHING
+            compressed_archive.close()
 
     # Post-processing operations.
     if post_file_processing == FileOperations.COPY:
         destination_archive_filename = f"{destination_directory}{current_relative_path}{filename}"
-        logger.info(f":{process_pid}: Copying from [{current_path}{filename}] to target [{destination_archive_filename}]")
+        logger.info(f":PID-{process_pid}: Copying from [{current_path}{filename}] to target [{destination_archive_filename}]")
         os.makedirs(f"{destination_directory}{current_relative_path}", exist_ok=True)
         shutil.copy(f"{current_path}{filename}", destination_archive_filename, follow_symlinks=True)
 
+    empty_file.close()
     destination_file_size = os.path.getsize(f"{destination_archive_filename}")
-    logger.info(f":{process_pid}: - Process completed on source file [{current_path}{filename}] {human_readable_bytes(file_size)} to destination [{destination_archive_filename}] {human_readable_bytes(destination_file_size)}")
+    logger.info(f":PID-{process_pid}: - Process completed on source file [{current_path}{filename}] {human_readable_bytes(file_size)} to destination [{destination_archive_filename}] {human_readable_bytes(destination_file_size)}")
     the_semaphore.release()
 
 
@@ -146,6 +148,7 @@ def compress_files(source_directory: str, destination_directory: str, processes:
         logging.info(f"-> Current path: [{path}]")
         current_relative_path = path.replace(source_directory, "").lstrip("/")
         logging.info(f"-> Current relative path: [{current_relative_path}]")
+        os.makedirs(f"{destination_directory}{current_relative_path}", exist_ok=True)
 
         for the_file in files:
             number_of_files += 1
@@ -186,8 +189,6 @@ if __name__ == "__main__":
     if args.processes is None or args.processes == 0:
         args.processes = max(os.cpu_count() - 1, 1)
         logging.info(f"Auto-setting number of multiprocessing threads to: {args.processes}.")
-
-    logging.warning(f"Note: Empty directories inside archives and directories are not transferred.")
 
     os.makedirs(args.destination_directory, exist_ok=True)
     logging.info(f"Created destination directory: [{args.destination_directory}]")
